10c73e369f32613ea6ad89015b76a72286757dd1
seungrii <choevictory@gmail.com>
2022-10-14 16:38:20 +0900

    테스트 커밋

---
+++ semi00/src/main/java/com/kh/semi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.kh.semi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+	
+
+	public String test() {
+		return "";
+	}
+	
+}
