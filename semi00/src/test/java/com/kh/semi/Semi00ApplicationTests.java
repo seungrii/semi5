7de9afa0c2c06a278935f4f81f4da29e74d5fa27
seungrii <choevictory@gmail.com>
2022-10-14 16:36:34 +0900

    생성

---
+++ semi00/src/test/java/com/kh/semi/Semi00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.semi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semi00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
