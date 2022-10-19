4251b7028cda2ef98c297d8718293dedb114cad4
seungrii <choevictory@gmail.com>
2022-10-19 10:56:18 +0900

    생성

---
+++ semi5/src/main/java/com/victory/semi5/Semi5Application.java
@@ -0,0 +1,13 @@
+package com.victory.semi5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semi5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semi5Application.class, args);
+	}
+
+}
---
+++ semi5/src/test/java/com/victory/semi5/Semi5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victory.semi5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semi5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
