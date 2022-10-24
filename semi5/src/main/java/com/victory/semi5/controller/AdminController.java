e272b161aee732451999af13597c440cf90c1b3c
ra7280 <lhr7280@gmail.com>
2022-10-24 14:48:23 +0900

    admin계정 추가, 조회

--- semi5/src/main/java/com/victory/semi5/controller/AdminController.java
+++ semi5/src/main/java/com/victory/semi5/controller/AdminController.java
@@ -2,10 +2,12 @@ package com.victory.semi5.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.victory.semi5.entity.AdminDto;
 import com.victory.semi5.repository.AdminDao;
@@ -17,11 +19,13 @@ public class AdminController {
 	@Autowired
 	private AdminDao adminDao;
 	
+	//admin home
 	@GetMapping("/home")
 	public String home() {
 		return "admin/home";
 	}
 	
+	//admin 계정추가
 	@GetMapping("/addAdmin")
 	public String addAdmin() {
 		return "admin/addAdmin";
@@ -32,6 +36,36 @@ public class AdminController {
 		adminDao.addAdmin(adminDto);
 		return "redirect:addAdmin";
 	}
+	//계정추가 완료시, 추가완료 알람창 구현필요(js)
+	
+	//admin 계정조회
+	@GetMapping("/listAdmin")
+	public String listAdmin(
+			Model model,
+			@RequestParam(required = false) String type,
+			@RequestParam(required = false) String keyword) {
+		boolean isSearch = type != null && keyword != null;
+		if(isSearch) {
+			model.addAttribute("listAdmin", adminDao.selectList(type, keyword));
+		}
+		else {
+			model.addAttribute("listAdmin", adminDao.selectList());
+		}
+		return "admin/listAdmin";
+	}
+	@GetMapping("/detailAdmin")
+	public String datailAdmin(
+			Model model,
+			@RequestParam String adminID) {
+		AdminDto adminDto = adminDao.selectOne(adminID);
+		model.addAttribute("adminDto", adminDto);
+		return "admin/detailAdmin";
+	}
+	
+	//admin 계정수정
+	
+	
+	//admin 계정삭제
 	
 	
 }
--- semi5/src/main/java/com/victory/semi5/repository/AdminDaoImpl.java
+++ semi5/src/main/java/com/victory/semi5/repository/AdminDaoImpl.java
@@ -1,7 +1,11 @@
 package com.victory.semi5.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.victory.semi5.entity.AdminDto;
@@ -20,5 +24,43 @@ public class AdminDaoImpl implements AdminDao{
 		};
 		jdbcTemplate.update(sql, param);
 	}
+
+	private RowMapper<AdminDto> mapper = (rs, idx) -> {
+		return AdminDto.builder()
+					.adminId(rs.getString("admin_id"))
+					.adminPw(rs.getString("admin_pw"))
+					.employeeNo(rs.getInt("employee_no"))
+				.build();
+	};
+	@Override
+	public List<AdminDto> selectList() {
+		String sql = "select * from admin order by admin_id asc";
+		return jdbcTemplate.query(sql, mapper);
+	}
+	@Override
+	public List<AdminDto> selectList(String type, String keyword) {
+		String sql = "select * from admin "
+					+ "where instr(#1, ?) >0 "
+					+ "order by admin_in asc";
+		sql = sql.replace("#1", type);
+		Object[] param = {keyword};
+		return jdbcTemplate.query(sql, mapper, param);
+	}
+	
+	private ResultSetExtractor<AdminDto> extractor = (rs) -> {
+		return AdminDto.builder()
+					.adminId(rs.getString("admin_id"))
+					.adminPw(rs.getString("admin_pw"))
+					.employeeNo(rs.getInt("employee_no"))
+				.build();
+	};
+	@Override
+	public AdminDto selectOne(String adminId) {
+		String sql = "select * from admin "
+					+ "where admin_id = ?";
+		Object[] param = {adminId};
+		return jdbcTemplate.query(sql, extractor, param);
+	}
+
 
 }
