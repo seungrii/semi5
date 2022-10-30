67517ca957e69cee431da3186f00fff22081807b
boara <boara@DESKTOP-7O0CN6J>
2022-10-30 20:42:38 +0900

    머지 후 오류 제거

--- semi5/src/main/java/com/victory/semi5/controller/MovieController.java
+++ semi5/src/main/java/com/victory/semi5/controller/MovieController.java
@@ -5,20 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.victory.semi5.entity.CharacterDto;
-import com.victory.semi5.entity.GenreDto;
-import com.victory.semi5.entity.MovieDto;
 import com.victory.semi5.repository.CharacterDao;
 import com.victory.semi5.repository.GenreDao;
-import com.victory.semi5.repository.ImageDao;
 import com.victory.semi5.repository.MovieDao;
-import com.victory.semi5.vo.HashtagVO;
 
 
 @Controller
@@ -30,9 +22,6 @@ public class MovieController {
 	
 	@Autowired 
 	MovieDao movieDao;
-
-	@Autowired 
-	ImageDao imageDao;
 	
 	@Autowired 
 	CharacterDao characterDao;
