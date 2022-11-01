1bbaddf3cd3e9f9b263943c58f218252a5589b1a
boara <boara@DESKTOP-7O0CN6J>
2022-11-01 21:48:45 +0900

    중간커밋, 인터셉터

---
+++ semi5/src/main/java/com/victory/semi5/configuration/InterceptorConfiguration.java
@@ -0,0 +1,85 @@
+package com.victory.semi5.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.victory.semi5.interceptor.AdminInterceptor;
+import com.victory.semi5.interceptor.MemberInterceptor;
+
+//스프링 설정파일
+//application.properties에서 설정하기 어려운 복잡한 설정을 구현
+//프로그램 동작 방식과 관련된 설정일 경우 특정 인터페이스를 상속 받아야함
+//interface WebMvcConfigurer
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+	
+	@Autowired 
+	private MemberInterceptor memberInterceptor;
+	@Autowired
+	private AdminInterceptor adminInterceptor;
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//registry에 추가하여 인터셉터가 작동하도록 설정
+		
+		
+		
+		//회원용 인터셉터
+		registry.addInterceptor(memberInterceptor)
+			.addPathPatterns(
+				
+					///////////////////////////////////////////////
+					//"/admin/**", 회원도 비회원도 못들어 오므로 패쓰
+					"/advance/list",
+					"/board/**",
+					"/user/**",
+					"/qna/**"
+					
+					
+					
+	)
+	.excludePathPatterns(//위의 주소에서 제외할 주소
+			
+	
+			
+			
+			///////////////////////////////
+			
+			"/board/list",
+			"/user/idFind",
+			"/user/join",
+			"/user/login",
+			"/user/pwFind",
+			"/qna/list"
+			
+			
+		
+			);
+		
+		//관리자용 인터셉터
+		registry.addInterceptor(adminInterceptor)
+					.addPathPatterns(//인터셉터가 감시할 주소...관리자만이 들어 갈 수 있는 곳
+						//    "/guestbook/edit*",//방명록 수정페이지
+						"/admin/**"	,
+						"/notice/edit",
+						"/notice/write"
+					
+							
+							
+					
+					)
+					.excludePathPatterns(//위의 주소에서 제외할 주소
+						//     "/music/list",//음원 목록
+							
+							
+							
+							
+							
+						
+					);
+	
+			
+	}
+}
+
---
+++ semi5/src/main/java/com/victory/semi5/interceptor/AdminInterceptor.java
@@ -0,0 +1,33 @@
+package com.victory.semi5.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.victory.semi5.constant.SessionConstant;
+
+
+
+@Component
+public class AdminInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		String mg = (String)session.getAttribute(SessionConstant.GRADE);
+		boolean admin = mg != null && mg.equals("관리자");
+		
+		if(admin) {//관리자라면
+			return true;//통과
+		}
+		else {//관리자가 아니라면
+			response.sendRedirect("/");//메인으로 리다이렉트
+			//response.sendError(403);//사용자에게 권한없음(403) 전송
+			return false;//차단
+		}
+	}
+}
